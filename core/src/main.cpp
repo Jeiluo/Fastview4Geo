6f24bd65e9d9f32489049f4948f1f8a25ef08cf1
DonaldTrump-coder <3279179450@qq.com>
2026-01-13 19:47:01 +0800

    fix bug

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -3,7 +3,7 @@
 
 int main() 
 {
-    TIFF* tif = TIFFOpen("D:/Projects/Fastview4Geo/data/TIFFExamples/BigTIFF.tif", "r");
+    TIFF* tif = TIFFOpen("../data/TIFFExamples/BigTIFF.tif", "r");
     TIFFClose(tif);
     std::cout<< "TIFF file read successfully."<< std::endl;
     return 0;
